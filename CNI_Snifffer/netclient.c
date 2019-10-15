6a7a0a9f20cd0b7a981ddfefea8242667fdc8e1c
Sneha Sundar <39394526+SnehaSundar@users.noreply.github.com>
2019-10-15 12:58:34 +0530

    Update netclient.c

--- CNI_Snifffer/netclient.c
+++ CNI_Snifffer/netclient.c
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[])
   
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-      if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+      if(inet_pton(AF_INET, "172.17.0.1", &serv_addr.sin_addr)<=0) 
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
